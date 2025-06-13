674393f9dd547077b43898bde8f8f0876d18063c
harshitabisht05 <harshitabisht0515@gmail.com>
2025-06-14 00:45:33 +0530

    Updated files

---
+++ app/layout.jsx
@@ -0,0 +1,21 @@
+import "./globals.css";
+import Header from "./components/Header";
+
+
+// Site metadata (can keep as plain object)
+export const metadata = {
+  title: "Sentienta",
+  description: "A clean and modern site inspired by Anduril",
+};
+
+// Root layout component
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className="antialiased">
+        <Header />
+        <main className="pt-20">{children}</main>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.jsx
@@ -0,0 +1,19 @@
+import Hero from "./components/Hero";
+import Projects from "./components/Projects";
+import ContactUS from "./components/ContactUS";
+import Footer from "./components/Footer";
+import './globals.css';
+
+
+
+export default function Home() {
+  return (
+    <>
+    
+      <Hero />
+      <Projects />
+      <ContactUS />
+       <Footer />
+    </>
+  );
+}
